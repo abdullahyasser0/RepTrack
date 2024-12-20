03d7e088a375878917053250e1f3a360b4fe9c19
ZiadMAlsawy <s-ziad.sawy@zewailcity.edu.eg>
2024-12-20 14:48:58 +0200

    Fixes #101

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path,include
 from . import views
-from databaseApi.views import signup,get_Users,get_Profiles,change_data,change_Password,get_Coaches,verify_token,profile_view,signup, login,addPost
+from databaseApi.views import signup,get_Users,get_Profiles,change_data,change_Password,get_Coaches,verify_token,profile_view,signup, login,addPost,addComment
 from users.views import signup_view,users,admins,coaches,dashboard,Equip,Payment,salesReport
 
 urlpatterns = [path('login/', views.login, name='login'),
@@ -17,12 +17,13 @@ urlpatterns = [path('login/', views.login, name='login'),
                 path('profilesData/',admins,name='profiles'),
                 path('changeDataForm/',change_data, name = 'changeDataForm'),
                 path('changePasswordForm/',change_Password, name = 'changePasswordForm'),
-
+                
                 path('verify-token/', verify_token, name='verify_token'),
                 path('Dashoard/',dashboard, name = 'Dashboard'),
                 path('Equip/',Equip, name = 'Equip'),
                 path('payment/',Payment, name = 'payment'),
                 path('addPost/',addPost, name = 'addPost'),
+                path('addComment/',addComment, name = 'addComment'),
                 path('salesReport/',salesReport, name = 'salesReport'),
                 path('user-dashboard/', views.user_dashboard, name='user_dashboard'),
                 path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
