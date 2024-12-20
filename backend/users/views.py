012506b54ffd10807466548edc4c16263be17cb7
Abdullah <s-abdullah.yasser@zewailcity.edu.eg>
2024-12-20 16:20:56 +0200

    redused imports

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
-from .forms import SimpleForm,SignupForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.http import JsonResponse
