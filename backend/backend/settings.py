7f3c359cf6ebaadeb8578c5db120063844400f5a
Abdullah Yasser <128793206+abdullahyasser0@users.noreply.github.com>
2024-12-20 21:56:59 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-fh9-$@*4t7c$5*x9on1-%z!#svehd&m_-egtpkby+1e1!1eul#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['reptrack-b8gqfdhsd8cwhgd8.westeurope-01.azurewebsites.net','127.0.0.1']
 # settings.py
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
