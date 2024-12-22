eafa8e5b16428ee3747abd9bfe5d186014986a94
Abdullah Yasser <128793206+abdullahyasser0@users.noreply.github.com>
2024-12-22 07:09:25 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -103,10 +103,13 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
